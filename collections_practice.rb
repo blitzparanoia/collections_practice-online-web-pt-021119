25d2df1a69f0f0a3eb00441f12273c421f07c49f
Irene Saez <bluemilk44@gmail.com>
2019-02-23 20:34:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
+def sort_array_asc(num)
+  num.sort
+end
 
+def sort_array_desc(num)
+  num.sort { |left ,right| right <=> left }
+end
+
+def sort_array_char_count(animals)
+  animals.sort {|left, right|}
+    left.length <=> right.length
+  end
+end
