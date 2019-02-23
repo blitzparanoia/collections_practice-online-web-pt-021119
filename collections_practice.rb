22d9bf3a881c50030ffe7658060b59f473f75ff0
Irene Saez <bluemilk44@gmail.com>
2019-02-23 21:05:06 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(num)
+  num.sort
+end
 
+def sort_array_desc(num)
+  num.sort { |left ,right| right <=> left }
+end
+
+def sort_array_char_count(animals)
+  animals.sort { |left,right| left.length <=> right.length }
+end
+
+def swap_elements(names)
+  names[1], names[2] = names[2], names [1]
+  names
+end
+
+def reverse_array(num)
+  num.reverse
+end
+
+def kesha_maker(name)
+  name.each do |change|
+    change[2] = "$"
+  end
+end
+
+def find_a(words)
+  words.find_all do |item|
+    item[0] == "a"
+  end
+end  
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum+=num
+end
+sum
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
